bcec8585efdeb3833024191d7cb8eb234911ebfc
Chris Lozzi <clozzi12@gmail.com>
2023-12-05 15:28:02 -0500

    all tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [n for n in num_list if n % 2 == 0]
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_list = [sen + "!" for sen in sentence_list]
+    return exclaimed_list
